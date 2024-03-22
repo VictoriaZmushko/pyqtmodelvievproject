4cec3b284c9e899fde59b9dda69d8fbc40aaaa5e
Victoria <Victory@n1866.BTI.DOMAIN>
2024-03-22 11:44:18 +0300

    create table v.2

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ createTableQuery = QSqlQuery()
 createTableQuery.exec(
     """
     CREATE TABLE contacts(
-    id INTEGER PRIMARY KEY AUTOINCREMENT INIQUE NOT NULL,
+    id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
     name VARCHAR(40) NOT NULL,
     job VARCHAR(50),
     email VARCHAR(40) NOT NULL
