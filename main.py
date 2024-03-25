6391b1ff554da2e57db4f587c52fc22dc568f4c3
Victoria <Victory@n1866.BTI.DOMAIN>
2024-03-25 17:06:42 +0300

    adding data

--- main.py
+++ main.py
@@ -2,26 +2,54 @@ import sys
 
 from PyQt5.QtSql import QSqlDatabase, QSqlQuery
 
-# Create the connection.
+# Create the connection
 con = QSqlDatabase.addDatabase("QSQLITE")
 con.setDatabaseName("contacts.sqlite")
 
-# Open the connection.
+# Open the connection
 if not con.open():
-    print("Database Error:%s"%con.lastError().databaseText())
-    sys.exit(1)
+   print("Database Error: %s" % con.lastError().databaseText())
+   sys.exit(1)
 
-# Create a query and execute it right away using .exec().
+# Create a query and execute it right away using .exec()
 createTableQuery = QSqlQuery()
 createTableQuery.exec(
-    """
-    CREATE TABLE contacts(
+  """
+  CREATE TABLE contacts (
     id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
     name VARCHAR(40) NOT NULL,
     job VARCHAR(50),
     email VARCHAR(40) NOT NULL
-    )
-    """
+  )
+  """
 )
 
 print(con.tables())
+# Creating a query for later execution using .prepare()
+insertDataQuery = QSqlQuery()
+insertDataQuery.prepare(
+  """
+  INSERT INTO contacts (
+    name,
+    job,
+    email
+  )
+  VALUES (?, ?, ?)
+  """
+)
+
+# Sample data
+data = [
+  ("Петя", "Senior Web Developer", "petya@example.com"),
+  ("Лера", "Project Manager", "lera@example.com"),
+  ("Денис", "Data Analyst", "denis@example.com"),
+  ("Катя", "Senior Python Developer", "ket@example.com"),
+]
+
+# Use .addBindValue() to insert data
+for name, job, email in data:
+   insertDataQuery.addBindValue(name)
+   insertDataQuery.addBindValue(job)
+   insertDataQuery.addBindValue(email)
+
+   insertDataQuery.exec()
